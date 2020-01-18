6c4108b6d60f3017d550a930a556a2dad7ad4d3d
Konstantin S <havendv@gmail.com>
2020-01-18 17:56:56 +1000

    Added dependencies restore

--- H.Dependencies.NuGet.Tests/NugetRestorerTests.cs
+++ H.Dependencies.NuGet.Tests/NugetRestorerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,7 +10,7 @@ namespace H.Dependencies.Tests
     public class NugetRestorerTests
     {
         [TestMethod]
-        public async Task ConnectTest()
+        public async Task RestoreRecursiveTest()
         {
             var folder = Path.Combine(Path.GetTempPath(), nameof(NugetRestorerTests));
 
@@ -18,12 +19,33 @@ namespace H.Dependencies.Tests
                 Directory.Delete(folder, true);
             }
 
-            await NugetRestorer.RestoreAsync(folder, "H.Pipes");
+            await NugetRestorer.RestoreRecursiveAsync(folder, 
+                "H.Pipes", 
+                ".NETStandard",
+                new Version(2, 1),
+                ignoredNames: new []{ "NETStandard.Library" });
 
             Console.WriteLine("New files:");
             foreach (var path in Directory.EnumerateFiles(folder, "*.*", SearchOption.AllDirectories))
             {
                 Console.WriteLine($" - {path.Replace(folder, string.Empty)}");
+            }
+        }
+
+        [TestMethod]
+        public async Task GetDependenciesTest()
+        {
+            var dependencies = await NugetRestorer.GetDependenciesRecursiveAsync(
+                "H.Pipes", 
+                ".NETStandard", 
+                new Version(2, 1),
+                ignoredNames: new [] { "NETStandard.Library" });
+
+            Trace.WriteLine("Dependencies:");
+
+            foreach (var dependency in dependencies)
+            {
+                Trace.WriteLine($" - {dependency.Id}: {dependency.VersionRange}");
             }
         }
     }
--- H.Dependencies.NuGet/NugetRestorer.cs
+++ H.Dependencies.NuGet/NugetRestorer.cs
@@ -4,19 +4,28 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Frameworks;
 using NuGet.PackageManagement;
+using NuGet.Packaging.Core;
 using NuGet.ProjectManagement;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 using NuGet.Resolver;
+using NuGet.Versioning;
 
 namespace H.Dependencies
 {
     public static class NugetRestorer
     {
-        public static async Task RestoreAsync(string folder, string packageName, CancellationToken cancellationToken = default)
+        #region Public methods
+
+        public static async Task<PackageIdentity> RestoreAsync(
+            string folder, 
+            string packageName, 
+            string? version = default, 
+            CancellationToken cancellationToken = default)
         {
             Directory.CreateDirectory(folder);
 
@@ -30,21 +39,8 @@ namespace H.Dependencies
             var packageSourceProvider = new PackageSourceProvider(settings);
             var sourceRepositoryProvider = new SourceRepositoryProvider(packageSourceProvider, providers);
 
-            var search = await sourceRepository.GetResourceAsync<PackageSearchResource>(cancellationToken);
-            var packages = await search.SearchAsync(packageName, new SearchFilter(true)
-            {
-                IncludeDelisted = true,
-            }, 0, 10, null, cancellationToken);
+            var identity = await GetIdentityAsync(sourceRepository, packageName, version, cancellationToken);
             
-            INuGetProjectContext projectContext = new EmptyNuGetProjectContext();
-            var resolutionContext = new ResolutionContext(
-                DependencyBehavior.Ignore,
-                true,
-                true,
-                VersionConstraints.None);
-
-            var package = packages.First();
-
             var project = new FolderNuGetProject(folder);
             var packageManager = new NuGetPackageManager(sourceRepositoryProvider, settings, folder)
             {
@@ -52,19 +48,144 @@ namespace H.Dependencies
             };
             await packageManager.InstallPackageAsync(
                 project,
-                package.Identity,
-                resolutionContext,
-                projectContext,
+                identity,
+                new ResolutionContext(
+                    DependencyBehavior.Ignore,
+                    true,
+                    true,
+                    VersionConstraints.None),
+                new EmptyNuGetProjectContext(), 
                 sourceRepository,
                 new List<SourceRepository>(),
                 cancellationToken);
 
-            var infos = await packageManager.GetInstalledPackagesDependencyInfo(project, cancellationToken, true);
-            var test = await sourceRepository.GetResourceAsync<DependencyInfoResource>(cancellationToken);
-            var www = await test.ResolvePackage(package.Identity, NuGetFramework.AnyFramework,
-                new SourceCacheContext(), new ProjectContextLogger(projectContext), cancellationToken);
-
-            var tes2t = test;
+            return identity;
         }
+
+        public static async Task RestoreRecursiveAsync(
+            string folder,
+            string packageName,
+            string targetFramework,
+            Version targetFrameworkVersion,
+            string? version = default,
+            int maxDepth = 99,
+            IEnumerable<string>? ignoredNames = default,
+            CancellationToken cancellationToken = default)
+        {
+            var identity = await RestoreAsync(folder, packageName, version, cancellationToken);
+
+            var dependencies = await GetDependenciesRecursiveAsync(
+                packageName,
+                targetFramework, 
+                targetFrameworkVersion,
+                identity.Version.OriginalVersion,
+                maxDepth,
+                ignoredNames, 
+                cancellationToken);
+
+            await Task.WhenAll(dependencies
+                .Select(async dependency => await RestoreAsync(
+                    folder, 
+                    dependency.Id, 
+                    dependency.VersionRange.MinVersion.OriginalVersion, 
+                    cancellationToken)));
+        }
+
+        public static async Task<IList<PackageDependency>> GetDependenciesAsync(
+            SourceRepository sourceRepository,
+            string packageName,
+            string targetFramework,
+            Version targetFrameworkVersion,
+            string? version = default,
+            CancellationToken cancellationToken = default)
+        {
+            var identity = await GetIdentityAsync(sourceRepository, packageName, version, cancellationToken);
+            var dependencyInfoResource = await sourceRepository.GetResourceAsync<DependencyInfoResource>(cancellationToken);
+            var dependencyInfo = await dependencyInfoResource.ResolvePackage(
+                identity, 
+                new NuGetFramework(targetFramework, targetFrameworkVersion), 
+                new SourceCacheContext(), 
+                new NullLogger(), 
+                cancellationToken);
+
+            return dependencyInfo
+                .Dependencies
+                .ToList();
+        }
+
+        public static async Task<IList<PackageDependency>> GetDependenciesRecursiveAsync(
+            string packageName,
+            string targetFramework,
+            Version targetFrameworkVersion,
+            string? version = default,
+            int maxDepth = 99,
+            IEnumerable<string>? ignoredNames = default,
+            CancellationToken cancellationToken = default)
+        {
+            var dependencies = new List<PackageDependency>();
+            var sourceRepository = GetSourceRepository();
+
+            var identity = await GetIdentityAsync(sourceRepository, packageName, version, cancellationToken);
+            var currentDependencies = new List<PackageDependency>
+            {
+                new PackageDependency(identity.Id, new VersionRange(identity.Version))
+            };
+            for (var i = 0; i < maxDepth && currentDependencies.Any(); i++)
+            {
+                var tasks = currentDependencies
+                    .Select(async dependency => await GetDependenciesAsync(
+                        sourceRepository,
+                        dependency.Id,
+                        targetFramework,
+                        targetFrameworkVersion, 
+                        dependency.VersionRange.MinVersion.OriginalVersion,
+                        cancellationToken));
+
+                currentDependencies = (await Task.WhenAll(tasks))
+                    .SelectMany(dependency => dependency)
+                    .Where(dependency => ignoredNames?.Contains(dependency.Id) != true)
+                    .ToList();
+
+                dependencies.AddRange(currentDependencies);
+            }
+
+            return dependencies.Distinct(new PackageDependencyComparer()).ToList();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static async Task<PackageIdentity> GetIdentityAsync(
+            SourceRepository sourceRepository,
+            string packageName,
+            string? version = default,
+            CancellationToken cancellationToken = default)
+        {
+            if (version != null)
+            {
+                return new PackageIdentity(packageName, NuGetVersion.Parse(version));
+            }
+
+            var search = await sourceRepository.GetResourceAsync<PackageSearchResource>(cancellationToken);
+            var packages = await search.SearchAsync(packageName, new SearchFilter(true)
+            {
+                IncludeDelisted = true,
+            }, 0, 10, null, cancellationToken);
+
+            return packages.First().Identity;
+        }
+
+        private static SourceRepository GetSourceRepository()
+        {
+            var providers = new List<Lazy<INuGetResourceProvider>>();
+            providers.AddRange(Repository.Provider.GetCoreV3());
+
+            var packageSource = new PackageSource("https://api.nuget.org/v3/index.json");
+
+            return new SourceRepository(packageSource, providers);
+        }
+
+        #endregion
     }
 }
